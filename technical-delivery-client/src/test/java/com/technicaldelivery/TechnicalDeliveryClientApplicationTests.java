b1c3b5b560c3ff46b347344a282c7a18fbaad25f
Bruno de Farias Bezerra Soares <bbezerra@ibm.com>
2021-01-01 14:12:00 -0300

    Criando o projeto de Micro Servico de Cliente

---
+++ technical-delivery-client/src/test/java/com/technicaldelivery/TechnicalDeliveryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technicaldelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalDeliveryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
