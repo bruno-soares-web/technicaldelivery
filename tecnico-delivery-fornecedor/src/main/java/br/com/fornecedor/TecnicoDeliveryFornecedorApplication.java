3df85f5a33fc4ac33529ff23f5547361e6209662
Bruno de Farias Bezerra Soares <bbezerra@ibm.com>
2021-01-20 15:02:34 -0300

    Construindo Micro Servico de Fornecedor

---
+++ tecnico-delivery-fornecedor/src/main/java/br/com/fornecedor/TecnicoDeliveryFornecedorApplication.java
@@ -0,0 +1,13 @@
+package br.com.fornecedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnicoDeliveryFornecedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnicoDeliveryFornecedorApplication.class, args);
+	}
+
+}
